49b4aab181ab9a6083717dcce85dc756e8b8bebd
valerie synenko <valerii.synenko@gmail.com>
2025-12-10 21:59:38 +0100

    docs: Add docstring user request model

--- src/models/responses/register_user_response.py
+++ src/models/responses/register_user_response.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel, field_validator
 
 
 class RegisterUserResponse(BaseModel):
+    """
+    The model for register user response.
+    The response has only one field - id.
+    """
+
     id: str
 
     @field_validator("id")
