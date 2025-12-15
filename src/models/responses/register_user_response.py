b91f11d614f83bf3836529ad4674ea7032d9efa4
valerie synenko <valerii.synenko@gmail.com>
2025-12-15 20:59:03 +0100

    refactor: Add field with len instead of field validator

--- src/models/responses/register_user_response.py
+++ src/models/responses/register_user_response.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field
 
 
 class RegisterUserResponseModel(BaseModel):
@@ -7,10 +7,4 @@ class RegisterUserResponseModel(BaseModel):
     The response has only one field - id.
     """
 
-    id: str
-
-    @field_validator("id")
-    def validate_id(cls, v):
-        if len(v) != 24 and type(v) != str:
-            raise ValueError(f"id must be string with 24 characters, but got '{v}' ({len(v)} characters).")
-        return v
+    id: str = Field(min_length=24, max_length=24)
