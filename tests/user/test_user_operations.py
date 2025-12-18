6f9493fe7e0a7116765757bc6564d9f7e8ea2256
valerie synenko <valerii.synenko@gmail.com>
2025-12-18 20:58:58 +0100

    feat: Add allure steps for test

--- tests/user/test_user_operations.py
+++ tests/user/test_user_operations.py
@@ -14,6 +14,8 @@ Typical usage example:
     pytest tests/test_user_operations.py::TestUserOperations::test_user_registration
 """
 
+import allure
+
 from src.models.requests.register_user_requests import RegisterUserRequestsModel
 from src.models.responses.register_user_response import RegisterUserResponseModel
 
@@ -22,16 +24,19 @@ class TestUserOperations:
 
     def test_user_registration_with_valid_creds(self, user_client, faker):
 
-        user_model = RegisterUserRequestsModel(
-            username=faker.user_name(),
-            email=faker.email(),
-            password=faker.password(),
-        )
+        with allure.step("Step 1: Register new user"):
+            user_model = RegisterUserRequestsModel(
+                username=faker.user_name(),
+                email=faker.email(),
+                password=faker.password(),
+            )
 
-        response = user_client.user_register(user_model=user_model)
+            response = user_client.user_register(user_model=user_model)
 
-        assert (
-            response.status_code == 200
-        ), f"the request with {user_model.model_dump()} returned status code: {response.status_code}"
+        with allure.step("Step 2: Assert that status code is 200"):
+            assert (
+                response.status_code == 200
+            ), f"the request with {user_model.model_dump()} returned status code: {response.status_code}"
 
-        RegisterUserResponseModel.model_validate(response.json())
+        with allure.step("Step 3: Validate response body"):
+            RegisterUserResponseModel.model_validate(response.json())
